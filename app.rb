171ae45dc6fa6a798fcc4a0409ec1c30456ed319
Leon <hualiang.tian@gmail.com>
2021-05-31 15:26:58 -0400

    commit tree 6a65b3f47c4a554cbdc96e4ecb7d6aa24ec8f903

--- app.rb
+++ app.rb
@@ -4,6 +4,28 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+    
+
+    post '/pirates' do
+      # binding.pry
+      # @pirate = Pirate.new(name: params[:pirate][:name], weight: params[:pirate][:weight], height: params[:pirate][:height])
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |ship_data|
+        Ship.new(ship_data)    
+      end
+      
+      erb :'pirates/show'
+    #  binding.pry
+    end
+    #
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+    PIRATES =[]
+
+    attr_accessor :name, :weight, :height
+    
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+        PIRATES << self
+    end
+
+    def self.all
+        PIRATES
+    end
+
+    def self.clear
+        PIRATES.clear
+    end
+
 end
